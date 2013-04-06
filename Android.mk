1ab44ccbc1ff77b48d6913793e71c7f92a1b584c
Jeff Sharkey <jsharkey@android.com>
2013-04-05 17:13:05 -0700

    Include JNI in both system and unbundled apps.
    Change-Id: Ib7767c9c0be07476960f6cf3fa8f15d0ded00ff8

--- Android.mk
+++ Android.mk
@@ -7,7 +7,14 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
-LOCAL_JNI_SHARED_LIBRARIES := libjni_terminal
+# If this is an unbundled build (to install seprately) then include
+# the libraries in the APK, otherwise just put them in /system/lib and
+# leave them out of the APK
+ifneq (,$(TARGET_BUILD_APPS))
+    LOCAL_JNI_SHARED_LIBRARIES := libjni_terminal
+else
+    LOCAL_REQUIRED_MODULES := libjni_terminal
+endif
 
 # TODO: enable proguard once development has settled down
 #LOCAL_PROGUARD_FLAG_FILES := proguard.flags
