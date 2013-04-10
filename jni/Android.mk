a816285f9c02e687268e76f02896ada33398b0dd
Ying Wang <wangying@google.com>
2013-04-10 10:38:24 -0700

    Add liblog.
    Bug: 8580410
    Change-Id: I345b43f305dc99a29da73e1f08185af5741eae76

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,13 +5,14 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     jni_init.cpp \
     com_android_terminal_Terminal.cpp \
-	forkpty.cpp
+    forkpty.cpp
 
 LOCAL_C_INCLUDES += \
     external/libvterm/include \
     libcore/include
 
 LOCAL_SHARED_LIBRARIES := \
+    liblog \
     libutils \
     libnativehelper
 
