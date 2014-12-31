bb6bb3f5e7e67d1ed734c3abc0d9303a6ed63021
Tom Marshall <tdm@cyngn.com>
2014-12-31 10:51:01 -0800

    Show cursor
    Change-Id: I31e99d20f3c0fa887f057b352e69969cb805016e

--- src/com/android/terminal/TerminalLineView.java
+++ src/com/android/terminal/TerminalLineView.java
@@ -61,7 +61,8 @@ public class TerminalLineView extends View {
 
         final TerminalMetrics m = mMetrics;
 
-        for (int col = 0; col < cols;) {
+        int col;
+        for (col = 0; col < cols;) {
             mTerm.getCellRun(row, col, m.run);
 
             m.bgPaint.setColor(m.run.bg);
@@ -81,5 +82,14 @@ public class TerminalLineView extends View {
 
             col += m.run.colSize;
         }
+
+        if (mTerm.getCursorVisible() && mTerm.getCursorRow() == row) {
+            col = mTerm.getCursorCol();
+            canvas.save();
+            canvas.translate(col * m.charWidth, 0);
+            canvas.drawRect(0, 0, m.charWidth, m.charHeight, m.cursorPaint);
+            canvas.restore();
+        }
+
     }
 }
