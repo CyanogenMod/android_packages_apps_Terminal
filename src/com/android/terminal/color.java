5395f20ca9096128fb20fc20646febbb198256c6
Stephen Bird <sbird@cyngn.com>
2015-06-27 15:57:46 -0700

    MOAR RAINBOW
    Change-Id: I39fda54201f48d1fa175e2171d33a8bafde00a22

---
+++ src/com/android/terminal/color.java
@@ -0,0 +1,18 @@
+package com.android.terminal;
+
+/**
+ * Created by bird on 6/27/15.
+ */
+public class color {
+
+    int r;
+    int g;
+    int b;
+
+    public color(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+}
