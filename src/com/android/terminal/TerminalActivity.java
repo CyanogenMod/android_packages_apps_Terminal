114b09911d2d345b68b6d0cc1420c875de016c7f
jrizzoli <joey@cyanogenmoditalia.it>
2015-11-17 16:28:40 +0100

    Terminal: add runtime permissions
    Change-Id: I31a8dd691ef2d2e300c4205278f2bff530b14b11
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- src/com/android/terminal/TerminalActivity.java
+++ src/com/android/terminal/TerminalActivity.java
@@ -18,6 +18,7 @@ package com.android.terminal;
 
 import static com.android.terminal.Terminal.TAG;
 
+import android.Manifest;
 import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.content.ComponentName;
@@ -26,10 +27,13 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
@@ -208,6 +212,13 @@ public class TerminalActivity extends Activity {
 
         ViewGroup root = (ViewGroup) findViewById(R.id.root);
         root.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+        final int REQUEST_WRITE_STORAGE=51;
+
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_WRITE_STORAGE);
+        }
     }
 
     @Override
